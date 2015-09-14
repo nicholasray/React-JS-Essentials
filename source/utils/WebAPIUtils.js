47ffe19ee420101b8a45fa3dc19521c78b723fff
Nicholas Ray <nrayemail@gmail.com>
2015-09-14 00:24:53 -0600

    Tutorial completed

---
+++ source/utils/WebAPIUtils.js
@@ -0,0 +1,11 @@
+var SnapkiteStreamClient = require('snapkite-stream-client');
+var TweetActionCreators = require('../actions/TweetActionCreators');
+require('../actions/TweetActionCreators');
+
+function initializeStreamOfTweets() {
+  SnapkiteStreamClient.initializeStream(TweetActionCreators.receiveTweet);
+}
+
+module.exports = {
+  initializeStreamOfTweets: initializeStreamOfTweets
+}
